70f188a42a551219fde4ded75e1852dbe70678ae
Yahia Eldeib <yeldeib@ufl.edu>
2020-07-19 16:28:45 -0400

    Project initialization

---
+++ src/main/java/com/cgi/heycanvas/canvasgoogleintegration/CanvasGoogleIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.heycanvas.canvasgoogleintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasGoogleIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasGoogleIntegrationApplication.class, args);
+	}
+
+}
