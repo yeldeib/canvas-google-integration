8161244403a36ac2212bba7da84585d8b05e854d
Yahia Eldeib <yeldeib@ufl.edu>
2020-08-24 05:14:04 -0400

    Getting courses for current term

--- src/main/java/com/cgi/heycanvas/canvasgoogleintegration/controllers/CourseController.java
+++ src/main/java/com/cgi/heycanvas/canvasgoogleintegration/controllers/CourseController.java
@@ -1,20 +1,29 @@
 package com.cgi.heycanvas.canvasgoogleintegration.controllers;
 
 import com.cgi.heycanvas.canvasgoogleintegration.models.Course;
+import com.cgi.heycanvas.canvasgoogleintegration.models.Term;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CourseController {
 
     @RequestMapping("/courses")
-    public Course getCourses(){
+    public List<Course> getCourses(){
         RestTemplate restTemplate = new RestTemplate();
-        Course courses = restTemplate.getForObject("https://ufl.instructure.com/api/v1/courses/351105/?include=term&access_token=1016~YlEwEpaHo2wBsCkdXu82N9B8d6hU4Ttn4Bmdwv5DrjnlCMz5G2ZZi302J33FCGpJ", Course.class);
-
-        return courses;
+        Course[] courses = restTemplate.getForObject("https://ufl.instructure.com/api/v1/courses/?include=term&access_token=1016~YlEwEpaHo2wBsCkdXu82N9B8d6hU4Ttn4Bmdwv5DrjnlCMz5G2ZZi302J33FCGpJ", Course[].class);
+        List<Course> courseList = new ArrayList<>();
+        for(int i=0;i<courses.length;i++){
+            Course currCourse = courses[i];
+            Term currTerm = currCourse.getTerm();
+            if(currTerm.getName().equals("Spring 2020")){
+                courseList.add(currCourse);
+            }
+        }
+        return courseList;
     }
 }
